de0d7d6d46d2ec3e2f0fe7389d4c3e9bb0ebc902
Alana Prem <alanaprem001@gmail.com>
2026-08-14 13:30:41 +0530

    filename capitalization mismatch

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getDashboardStats } = require('../controllers/dashboardController');
-const { protect, authorize } = require('../middleware/AuthMiddleware');
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 // Dashboard routes are protected and require Admin role
 router.use(protect);
